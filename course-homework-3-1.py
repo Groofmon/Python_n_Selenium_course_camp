1749b503f1cf2b1c68e68f454155dfc21da6c955
İ. Enes Aktaşoğlu <120311002+Groofmon@users.noreply.github.com>
2023-03-21 19:17:19 +0300

    Add files via upload

---
+++ course-homework-3-1.py
@@ -0,0 +1,34 @@
+# 1. HTML Nedir?
+# Hiper Metin İşaretleme Dili (HTML, İngilizce HyperText Markup Language kelimelerinin baş harflerinin kısaltılmasıdır)
+# web sayfalarını oluşturmak için kullanılan standart metin işaretleme dilidir. Dilin son sürümü HTML5'tir.
+# HTML, bir programlama dili olarak tanımlanamaz. Zira HTML kodlarıyla kendi başına çalışan bir program yazılamaz.
+# Ancak bu dili yorumlayabilen programlar aracılığıyla çalışabilen programlar yazılabilir. Programlama dili
+# denilememesinin nedeni tam olarak budur. Temel gereği yazı, görüntü, video gibi değişik verileri ve bunları içeren
+# sayfaları birbirine basitçe bağlamak, buna ek olarak söz konusu sayfaların web tarayıcısı yazılımları tarafından
+# düzgün olarak görüntülenmesi için gerekli kuralları belirlemektir. HTML kodunu web tarayıcıları okur, yorumlar ve
+# görsel hale dönüştürürler, dolayısıyla aynı HTML kodunun farklı tarayıcılarda farklı sonuç vermesi olasıdır. CSS ve
+# JavaScript ile beraber kullanıldığında HTML vasıtasıyla görsel ve dinamik web siteleri yaratılabilir.
+########################################################################################################################
+# 2. HTML Locators nedir?
+# HTML locators, web sayfalarındaki belirli bir öğeye ulaşmak için kullanılan tanımlayıcılardır. Bu locators,
+# web sayfasındaki farklı öğeleri, örneğin metin kutuları, düğmeler, resimler ve diğer etkileşimli öğeleri tanımlamak için kullanılır.
+# Web sayfalarındaki öğelere erişmek için birçok farklı locator türü vardır. Bazı yaygın HTML locators örnekleri şunlardır:
+# ID: Bir öğenin benzersiz bir kimliğini belirtir.
+# Class: Bir öğenin bir sınıfını belirtir.
+# Name: Bir öğenin adını belirtir.
+# Tag name: Bir öğenin etiket adını belirtir (örneğin, "div" veya "input").
+# CSS selector: Bir öğenin stil özelliklerine göre seçimini belirtir.
+# Bu locators, otomasyon testleri yaparken veya web sayfalarında öğeleri seçmek için kullanılırken önemli bir rol oynarlar.
+########################################################################################################################
+# 3. HTML Actions nelerdir?
+
+# click(): Bir öğeye tıklamak için kullanılır. Örneğin, bir düğmeye, bağlantıya veya seçim kutusuna tıklamak için kullanılır.
+# send_keys(): Bir öğeye yazı yazmak veya metin girişi yapmak için kullanılır. Örneğin, bir metin kutusuna kullanıcı adı veya parola yazmak için kullanılır.
+# clear(): Bir öğenin içeriğini temizlemek için kullanılır. Örneğin, bir metin kutusundaki varsayılan metni temizlemek için kullanılır.
+# get(): Belirli bir URL'ye gitmek için kullanılır.
+# submit(): Bir formu göndermek için kullanılır. Örneğin, bir formu doldurduktan sonra göndermek için kullanılır.
+# maximize_window(): Tarayıcı penceresini tam ekran yapmak için kullanılır.
+# switch_to(): Sayfa içinde farklı çerçeveler veya pencereler arasında geçiş yapmak için kullanılır.
+# find_element(): Belirli bir öğeyi bulmak için kullanılır. Örneğin, bir metin kutusunu veya bir düğmeyi bulmak için kullanılır.
+# get_attribute(): Bir öğenin belirli bir özniteliğinin değerini almak için kullanılır. Örneğin, bir düğmenin etiket adını veya bir görüntünün kaynak URL'sini almak için kullanılır.
+# is_displayed(): Bir öğenin görüntülenip görüntülenmediğini kontrol etmek için kullanılır. Örneğin, bir düğmenin veya bir metin kutusunun sayfada görünüp görünmediğini kontrol etmek için kullanılır.
